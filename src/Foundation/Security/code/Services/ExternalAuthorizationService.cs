77e9c5a4290021c2ac1c48b9b460267c5a92d49f
Viacheslav Sedletskyi <viacheslav.s@sohohouse.com>
2024-03-02 19:19:01 +0200

    Add external auth provider which will control specific logic and security model of external auth system

--- src/Foundation/Security/code/Services/ExternalAuthorizationService.cs
+++ src/Foundation/Security/code/Services/ExternalAuthorizationService.cs
@@ -1,29 +1,39 @@
-﻿using DreamTeam.Foundation.Security.Providers;
-using Sitecore.Data.Items;
+﻿using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Security.AccessControl;
 using Sitecore.Security.Accounts;
+using DreamTeam.Foundation.Extensions;
+using DreamTeam.Foundation.Security.Providers;
 using Sitecore.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
-using DreamTeam.Foundation.Extensions;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using DreamTeam.Foundation.Security.Exceptions;
-using Sitecore.Configuration;
-using System;
-using System.Runtime.Caching;
-using DreamTeam.Foundation.Security.Helpers;
-using Sitecore.Data;
-using DreamTeam.Foundation.Security.Entitlement.Entities;
 
 namespace DreamTeam.Foundation.Security.Services
 {
     public class ExternalAuthorizationService : IExternalAuthorizationService
     {
+        /// <summary>
+        /// Take place for different security auth providers under this variable
+        /// </summary>
+        private static IExternalAuthorizationSystemProvider _externalAuthorizationProvider => ServiceLocator.ServiceProvider.GetService<IExternalAuthorizationSystemProvider>();
+
         public AccessResult GetAccess(ISecurable entity, Account account)
         {
             Assert.ArgumentNotNull(account, "account");
+
+            if (entity is Item entityItem && entityItem.InheritsFrom(Templates._EncourageByEntitlements.ID) && !entityItem.IsTemplateItem())
+            {
+                //TODO: Possible to place caching mehanism
+
+                var user = (account as User) ?? User.FromName(account.Name, false);
+                var isPermit = _externalAuthorizationProvider.IsUserAuthorizedToGetSpecificItemAccess(entityItem, user);
+
+                var accessResult = isPermit ? new AccessResult(AccessPermission.Allow, new AccessExplanation(Constants.GrantAccessExplanationByEAS))
+                                            : new AccessResult(AccessPermission.Deny, new AccessExplanation(Constants.DenyAccessExplanationByEAS));
+
+                //TODO: place accessResult to the cache for faster exit from code
+
+                return accessResult;
+            }
 
             return new AccessResult(AccessPermission.NotSet, new AccessExplanation("Skiped by External Authorization system due to lack of entitlement restrictions or no data for particular item."));
         }
--- src/Foundation/Security/code/Templates.cs
+++ src/Foundation/Security/code/Templates.cs
@@ -1,7 +1,17 @@
-﻿namespace DreamTeam.Foundation.Security
+﻿using Sitecore.Data;
+
+namespace DreamTeam.Foundation.Security
 {
     public static class Templates
     {
+        public struct _EncourageByEntitlements
+        {
+            public static readonly ID ID = new ID("{30D1BE63-E408-41F7-8B5E-3E0700B791DA}");
+        }
 
+        public struct _EncourageByEntitlementsInDirectly
+        {
+            public static readonly ID ID = new ID("{94A51317-D978-4BA9-A846-D55E5A73EBA6}");
+        }
     }
 }
