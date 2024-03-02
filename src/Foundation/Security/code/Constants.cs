0ae1137810db3bc5e8464f541896f020dedb6d1e
Viacheslav Sedletskyi <viacheslav.s@sohohouse.com>
2024-03-02 20:43:31 +0200

    Add missed Constants.cs file

--- src/Foundation/Security/code/Constants.cs
+++ src/Foundation/Security/code/Constants.cs
@@ -2,5 +2,7 @@
 {
     public static class Constants
     {
+        public const string GrantAccessExplanationByEAS = "Access was granted by External Authorization system";
+        public const string DenyAccessExplanationByEAS = "Access was restricted by External Authorization system";
     }
 }
