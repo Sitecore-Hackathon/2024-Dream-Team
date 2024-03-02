172a7d61a9723e83be7beb675ee473e3dcd1606f
Viacheslav Sedletskyi <viacheslav.s@sohohouse.com>
2024-03-02 18:04:29 +0200

    use keySelector to provide property name

--- src/Foundation/Extensions/code/EnumarableExtensions.cs
+++ src/Foundation/Extensions/code/EnumarableExtensions.cs
@@ -5,13 +5,13 @@ namespace DreamTeam.Foundation.Extensions
 {
     public static class EnumarableExtensions
     {
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, TKey targetKeyValue)
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
         {
             HashSet<TKey> seenKeys = new HashSet<TKey>();
 
             foreach (TSource element in source)
             {
-                if (seenKeys.Add(targetKeyValue))
+                if (seenKeys.Add(keySelector(element)))
                 {
                     yield return element;
                 }
