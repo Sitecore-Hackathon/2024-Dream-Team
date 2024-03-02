d731384efb4ee9811f183d35f6b6bf25379c4674
Viacheslav Sedletskyi <viacheslav.s@sohohouse.com>
2024-03-02 18:52:36 +0200

    Add specific service which control external security authorization

--- src/Foundation/Security/code/AccessControl/ExternalAuthorizationProvider.cs
+++ src/Foundation/Security/code/AccessControl/ExternalAuthorizationProvider.cs
@@ -1,16 +1,27 @@
 ﻿using Sitecore.Data.DataProviders.Sql;
 using Sitecore.Security.AccessControl;
+using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Security.Accounts;
+using Sitecore.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using DreamTeam.Foundation.Security.Services;
+using DreamTeam.Foundation.Extensions;
 
 namespace DreamTeam.Foundation.Security.AccessControl
 {
     public class ExternalAuthorizationProvider : SqlAuthorizationProvider
 	{
-		public ExternalAuthorizationProvider(SqlDataApi api) : base(api)
-        {
+		private static readonly IExternalSecurityConfigurationService _externalSecurityConfigurationService;
 
-        }
+		static ExternalAuthorizationProvider()
+        {
+            _externalSecurityConfigurationService = ServiceLocator.ServiceProvider.GetService<IExternalSecurityConfigurationService>();
+		}
+
+		public ExternalAuthorizationProvider(SqlDataApi api) : base(api) 
+		{
+		}
 
 		/// <summary>
 		/// Determines whether the specified operation is allowed.
@@ -33,11 +44,25 @@ namespace DreamTeam.Foundation.Security.AccessControl
 				return accessResult;
 			}
 
+			//Donotate Sitecore Security with External Security Provider
+			if (ShouldBeValidatedByExternalAuhtorizationProvider(accessResult, entity))
+			{
+                return accessResult;
+			}
+
 			accessResult = this.GetAccessResultFromCache(entity, account, accessRight) ?? this.GetAccessCore(entity, account, accessRight);
 			
 			this.AddAccessResultToCache(entity, account, accessRight, accessResult, PropagationType.Entity);
 
 			return accessResult;
 		}
+
+		private static bool ShouldBeValidatedByExternalAuhtorizationProvider(AccessResult accessResult, ISecurable entity)
+		{
+			if (!_externalSecurityConfigurationService.IsEntitlementFeatureEnabled())
+				return false;
+
+            return accessResult == null && entity is Item item && item.ID != item.TemplateID && item.Database.Name.ToLower() != "core" && !item.IsTemplateItem();
+        }
 	}
 }
