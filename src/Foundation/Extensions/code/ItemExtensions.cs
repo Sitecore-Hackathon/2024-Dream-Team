cdba77b282296f1e5f54d3529c2864dfe9516797
Viacheslav Sedletskyi <viacheslav.s@sohohouse.com>
2024-03-03 01:40:03 +0200

    code clean up

--- src/Foundation/Extensions/code/ItemExtensions.cs
+++ src/Foundation/Extensions/code/ItemExtensions.cs
@@ -1,31 +1,14 @@
-﻿namespace DreamTeam.Foundation.Extensions
-{
-    using Sitecore;
-    using Sitecore.Data;
-    using Sitecore.Data.Fields;
-    using Sitecore.Data.Items;
-    using Sitecore.Data.Managers;
-    using Sitecore.Data.Templates;
-    using Sitecore.Diagnostics;
-    using Sitecore.Links.UrlBuilders;
-    using Sitecore.Resources.Media;
-    using System;
-    using System.Collections.Generic;
-    using System.Collections.Specialized;
-    using System.Linq;
+﻿using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Data.Managers;
+using Sitecore.Data.Templates;
+using System;
 
+namespace DreamTeam.Foundation.Extensions
+{
     public static class ItemExtensions
     {
-        public static Item FirstChildInheritingFrom(this Item item, ID templateId)
-        {
-            return item?.Children.FirstOrDefault(i => i.InheritsFrom(templateId));
-        }
-
-        public static bool InheritsFrom(this Item item, ID[] templateIds)
-        {
-            return templateIds.Any(tid => item.Template.DoesTemplateInheritFrom(tid));
-        }
-
         public static bool InheritsFrom(this Item item, ID templateId)
         {
             return item.Template.DoesTemplateInheritFrom(templateId);
@@ -34,32 +17,6 @@
         public static bool IsTemplateItem(this Item item)
         {
             return item.TemplateID == TemplateIDs.Template || (item.Name.Equals("__Standard Values", StringComparison.InvariantCultureIgnoreCase) && item.Paths.Path.StartsWith("/sitecore/templates"));
-        }
-
-        public static bool IsDerived(this Item item, ID templateId)
-        {
-            if (item == null)
-            {
-                return false;
-            }
-
-            return !templateId.IsNull && item.IsDerived(item.Database.Templates[templateId]);
-        }
-
-        private static bool IsDerived(this Item item, Item templateItem)
-        {
-            if (item == null)
-            {
-                return false;
-            }
-
-            if (templateItem == null)
-            {
-                return false;
-            }
-
-            var itemTemplate = TemplateManager.GetTemplate(item);
-            return itemTemplate != null && (itemTemplate.ID == templateItem.ID || itemTemplate.DescendsFrom(templateItem.ID));
         }
 
         public static bool DoesTemplateInheritFrom(this TemplateItem template, ID templateId)
